10d66751ab54b4ad9e685e79c337bf3a86481808
saiful-shant0 <sshanto.cse@gmail.com>
2022-08-28 13:43:21 +0600

    3user

---
+++ middleware/users/avaterUpload.js
@@ -0,0 +1,24 @@
+const uploader = require("../../utilities/singleUpload");
+
+function avaterUpload(req, res, next) {
+  const upload = uploader(
+    "avatars",
+    ["image/jpeg", "image/jpg", "image/png"],
+    1000000,
+    "Only .jpg, jpeg or .png format allowed!"
+  );
+  upload.any()(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        errors: {
+          avatar: {
+            msg: err.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
+}
+module.exports = avaterUpload;
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,32 @@
 //external
 const express = require("express");
+const { check } = require("express-validator");
 
 //internal imports
-const { getUsers } = require("../controller/usersController");
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controller/usersController");
 const deceoretHtmlResponse = require("../middleware/common/decoretHtmlResponse");
+const avaterUpload = require("../middleware/users/avaterUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middleware/users/userValidor");
 const router = express.Router();
 
 //login page
 router.get("/", deceoretHtmlResponse("Users"), getUsers);
+
+router.post(
+  "/",
+  avaterUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+// remove user
+router.delete("/:id", removeUser);
 
 module.exports = router;
